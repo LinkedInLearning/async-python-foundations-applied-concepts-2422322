2b92406b36ea74a47b25843fa5e0052942b545c0
Rony Sheer <ronysheer@gmail.com>
2021-06-30 19:14:15 +0300

    redis blog

--- CH_03_02.py
+++ CH_03_02.py
@@ -1,26 +1,28 @@
 import asyncio
 import aioredis
 import click
+import json
 
 
 async def main():
     redis = await aioredis.create_redis_pool("redis://localhost")
-    await redis.set("restaurant", "ASYNC PIZZA")
-    await redis.hmset_dict("item:1", {"name": "Pizza", "price": 5})
-    await redis.hmset_dict("item:2", {"name": "Salad", "price": 4})
-    await redis.hmset_dict("item:3", {"name": "Beverage", "price": 3})
+    await redis.flushall()
+    await redis.set("app", "Chat")
+    await redis.rpush("chat", json.dumps({"handle": "first_user", "message": "hey"}))
+    await redis.rpush(
+        "chat", json.dumps({"handle": "second_user", "message": "What's up?"})
+    )
+    await redis.rpush(
+        "chat", json.dumps({"handle": "first_user", "message": "all good!"})
+    )
 
-    restaurant = await redis.get("restaurant", encoding="utf-8")
-    first_item = await redis.hget("item:1", "name", encoding="utf-8")
-    first_item_price = await redis.hget("item:1", "price", encoding="utf-8")
-    items = await redis.hscan("name")
-    items = await redis.hscan("item:1")
+    app_name = await redis.get("app", encoding="utf-8")
+    click.secho(f" {app_name} ", fg="cyan", bold=True, bg="yellow")
     await redis.publish_json("feed", {"name": "Beverage", "price": 3})
-
-    print(items)
-
-    click.secho(f"    {restaurant}   ", fg="cyan", bold=True, bg="yellow")
-    click.secho(f"  {first_item} | ${first_item_price} ", fg="cyan")
+    chat_messages = await redis.lrange("chat", 0, -1, encoding="utf-8")
+    for message in chat_messages:
+        message = json.loads(message)
+        click.secho(f'  {message["handle"]} | {message["message"]} ', fg="cyan")
     redis.close()
     await redis.wait_closed()
 
