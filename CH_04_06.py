ba435e4138ba59802659f5a01699c856ca73e292
Rony Sheer <ronysheer@gmail.com>
2021-06-30 19:44:46 +0300

    chat and redis

--- CH_04_06.py
+++ CH_04_06.py
@@ -1,8 +1,10 @@
+import json
 import asyncio
 
 from quart import Quart, websocket, render_template
 from quart import g
 
+from quart_redis import RedisHandler, get_redis
 
 app = Quart(__name__)
 
@@ -13,10 +15,12 @@ connections = set()
 
 @app.websocket("/ws")
 async def ws():
+    redis = get_redis()
     connections.add(websocket._get_current_object())
     try:
         while True:
             message = await websocket.receive()
+            await redis.rpush("chat", json.dumps(message))
             send_coroutines = [connection.send(message) for connection in connections]
             await asyncio.gather(*send_coroutines)
     finally:
@@ -25,7 +29,10 @@ async def ws():
 
 @app.route("/")
 async def chat():
-    return await render_template("chat.html")
+    redis = get_redis()
+    messages = await redis.lrange("chat", 0, -1, encoding="utf-8")
+    messages = [json.loads(message) for message in messages]
+    return await render_template("chat_redis.html", messages=messages)
 
 
 # export QUART_APP=chat_app:app
